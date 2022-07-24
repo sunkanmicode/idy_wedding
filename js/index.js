6154d9698102a4f5b8ab0f2003f3891061cd2876
Sunkanmi <sunkanmi.oguntimehin@optisoft.ng>
2022-07-24 04:22:44 -0700

    adding friends images

--- js/index.js
+++ js/index.js
@@ -74,8 +74,180 @@ const getCountDownStart = () =>{
 const counter = setInterval(getCountDownStart, 1000);
 getCountDownStart();
 
+const photoGallery = document.querySelector(".photo-gallery");
+const bestiesGallery = document.querySelector(".besties-gallery");
+
+const besties = [
+  // {
+  //   id: 1,
+  //   images: "./img/photo (2).jpg",
+  //   name: "Noel"
+  // },
+  {
+    id: 2,
+    images: "./img/img20.jpg",
+    name: "Essien",
+  },
+  {
+    id: 3,
+    images: "./img/img21.jpg",
+    name: "Blessing",
+  },
+  {
+    id: 4,
+    images: "./img/lady1.jpg",
+    name: "Abasifreke",
+  },
+  {
+    id: 5,
+    images: "./img/lady2.jpg",
+    name: "Edidiong",
+  },
+  {
+    id: 6,
+    images: "./img/lady3.jpg",
+    name: "Mary",
+  },
+  {
+    id: 7,
+    images: "./img/lady4.jpg",
+    name: "Valentine",
+  },
+  {
+    id: 8,
+    images: "./img/lady5.jpg",
+    name: "Mary",
+  },
+  {
+    id: 9,
+    images: "./img/lady6.jpg",
+    name: "Nkese",
+  },
+  {
+    id: 10,
+    images: "./img/lady7.jpg",
+    name: "Naya",
+  },
+  {
+    id: 11,
+    images: "./img/lady8.jpg",
+    name: "Idara",
+  },
+  {
+    id: 12,
+    images: "./img/lady9.jpg",
+    name: "Josephine",
+  },
+  {
+    id: 12,
+    images: "./img/lady10.jpg",
+    name: "Rebecca",
+  },
+  {
+    id: 12,
+    images: "./img/lady11.jpg",
+    name: "Enobong",
+  },
+  {
+    id: 12,
+    images: "./img/lady12.jpg",
+    name: "Joyce",
+  },
+  // {
+  //   id: 12,
+  //   images: "./img/guy1b.jpg",
+  //   name: "Ohanele",
+  // },
+  {
+    id: 12,
+    images: "./img/guy2b.jpg",
+    name: "Ekong",
+  },
+  {
+    id: 12,
+    images: "./img/guy3.jpg",
+    name: "Udeh",
+  },
+  {
+    id: 12,
+    images: "./img/guy4.jpg",
+    name: "Stanley",
+  },
+  {
+    id: 12,
+    images: "./img/guy5.jpg",
+    name: "Ukeme",
+  },
+  {
+    id: 12,
+    images: "./img/guy6.jpg",
+    name: "Ohanele",
+  },
+  {
+    id: 12,
+    images: "./img/guy7.jpg",
+    name: "Clement",
+  },
+  {
+    id: 12,
+    images: "./img/guy8.jpg",
+    name: "Ekemini",
+  },
+  {
+    id: 12,
+    images: "./img/guy9.jpg",
+    name: "No name",
+  },
+  {
+    id: 12,
+    images: "./img/guy10.jpg",
+    name: "Akanifiok",
+  },
+  {
+    id: 12,
+    images: "./img/guy11.jpg",
+    name: "Akanimoh",
+  },
+  {
+    id: 12,
+    images: "./img/guy12.jpg",
+    name: "Adeyemi",
+  },
+  {
+    id: 12,
+    images: "./img/guy13.jpg",
+    name: "Ephraim",
+  },
+  {
+    id: 12,
+    images: "./img/guy14.jpg",
+    name: "Oyonumoh",
+  },
+  
+];
 
 
+window.addEventListener("DOMContentLoaded", () => {
+  displayPictures();
+});
+
+const displayPictures = () => {
+  // const displayPic = pictures.map((pic) => {
+  //   return `<div class="col-4">
+  //           <img src="${pic.image}" alt="">
+  //           </div> `;
+  // });
+  const displayPhoto = besties.map((photo) => {
+    return `<div class="col-3">
+            <img src="${photo.images}" alt="">
+            <p>${photo.name}</p>
+            </div> `;
+  });
+  showPhoto = displayPhoto.join("");
+  bestiesGallery.innerHTML = showPhoto;
+  showPicture = displayPic.join("");
+  photoGallery.innerHTML = showPicture;
+};
 
 
 
