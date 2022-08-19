f3ae613fda4e0ee33eaefd1a2ec599ef3a535072
Sunkanmi <sunkanmi.oguntimehin@optisoft.ng>
2022-08-18 22:26:52 -0700

    adding more pictures

--- js/index.js
+++ js/index.js
@@ -77,6 +77,146 @@ getCountDownStart();
 const photoGallery = document.querySelector(".photo-gallery");
 const bestiesGallery = document.querySelector(".besties-gallery");
 
+
+const pictures = [
+  {
+    id: 6,
+    category: "intro",
+    image: "./img/cp1.jpg",
+  },
+  {
+    id: 7,
+    category: "intro",
+    image: "./img/cp2.jpg",
+  },
+  {
+    id: 8,
+    category: "intro",
+    image: "./img/cp3.jpg",
+  },
+  {
+    id: 13,
+    category: "intro",
+    image: "./img/cp9.jpg",
+  },
+  {
+    id: 13,
+    category: "intro",
+    image: "./img/cp25.jpg",
+  },
+  {
+    id: 5,
+    category: "All",
+    image: "./img/cp13.jpg",
+  },
+  {
+    id: 5,
+    category: "All",
+    image: "./img/cp12.jpg",
+  },
+
+  {
+    id: 5,
+    category: "All",
+    image: "./img/cp24.jpg",
+  },
+  // {
+  //   id: 5,
+  //   category: "All",
+  //   image: "./img/cp22.jpg",
+  // },
+  {
+    id: 5,
+    category: "All",
+    image: "./img/cp15.jpg",
+  },
+
+  // {
+  //   id: 14,
+  //   category: "intro",
+  //   image: "./img/cp4.jpg",
+  // },
+  {
+    id: 15,
+    category: "All",
+    image: "./img/cp5.jpg",
+  },
+  {
+    id: 16,
+    category: "All",
+    image: "./img/cp6.jpg",
+  },
+
+  {
+    id: 3,
+    category: "All",
+    image: "./img/cp10.jpg",
+  },
+
+  {
+    id: 5,
+    category: "All",
+    image: "./img/cp14.jpg",
+  },
+  {
+    id: 5,
+    category: "All",
+    image: "./img/cp15.jpg",
+  },
+  {
+    id: 5,
+    category: "All",
+    image: "./img/cp16.jpg",
+  },
+  {
+    id: 5,
+    category: "All",
+    image: "./img/cp18.jpg",
+  },
+  {
+    id: 10,
+    category: "intro",
+    image: "./img/cp8.jpg",
+  },
+  {
+    id: 5,
+    category: "All",
+    image: "./img/cp17.jpg",
+  },
+
+  {
+    id: 4,
+    category: "All",
+    image: "./img/cp11.jpg",
+  },
+  {
+    id: 5,
+    category: "All",
+    image: "./img/cp19.jpg",
+  },
+  {
+    id: 5,
+    category: "All",
+    image: "./img/cp20.jpg",
+  },
+  {
+    id: 5,
+    category: "All",
+    image: "./img/cp21.jpg",
+  },
+  {
+    id: 9,
+    category: "intro",
+    image: "./img/cp7.jpg",
+  },
+
+  {
+    id: 5,
+    category: "All",
+    image: "./img/cp23.jpg",
+  },
+];
+
 const besties = [
   // {
   //   id: 1,
@@ -86,7 +226,7 @@ const besties = [
   {
     id: 2,
     images: "./img/img20.jpg",
-    name: "Essien",
+    name: "Idy",
   },
   {
     id: 3,
@@ -103,11 +243,7 @@ const besties = [
     images: "./img/lady2.jpg",
     name: "Edidiong",
   },
-  {
-    id: 6,
-    images: "./img/lady3.jpg",
-    name: "Mary",
-  },
+
   {
     id: 7,
     images: "./img/lady4.jpg",
@@ -142,6 +278,16 @@ const besties = [
     id: 12,
     images: "./img/lady10.jpg",
     name: "Rebecca",
+  },
+  {
+    id: 12,
+    images: "./img/lady30.jpg",
+    name: "Preshy",
+  },
+  {
+    id: 12,
+    images: "./img/lady31.jpg",
+    name: "Didi",
   },
   {
     id: 12,
@@ -210,6 +356,21 @@ const besties = [
   },
   {
     id: 12,
+    images: "./img/guy15.jpg",
+    name: "Kennedy",
+  },
+  {
+    id: 12,
+    images: "./img/guy17.jpg",
+    name: "Lampi",
+  },
+  {
+    id: 12,
+    images: "./img/guy30.jpg",
+    name: "Odiongenyene",
+  },
+  {
+    id: 12,
     images: "./img/guy12.jpg",
     name: "Adeyemi",
   },
@@ -223,20 +384,25 @@ const besties = [
     images: "./img/guy14.jpg",
     name: "Oyonumoh",
   },
-  
+  {
+    id: 6,
+    images: "./img/lady3.jpg",
+    name: "Mary",
+  },
 ];
 
 
 window.addEventListener("DOMContentLoaded", () => {
   displayPictures();
+  // play()
 });
 
 const displayPictures = () => {
-  // const displayPic = pictures.map((pic) => {
-  //   return `<div class="col-4">
-  //           <img src="${pic.image}" alt="">
-  //           </div> `;
-  // });
+  const displayPic = pictures.map((pic) => {
+    return `<div class="col-4">
+            <img src="${pic.image}" alt="">
+            </div> `;
+  });
   const displayPhoto = besties.map((photo) => {
     return `<div class="col-3">
             <img src="${photo.images}" alt="">
@@ -248,6 +414,12 @@ const displayPictures = () => {
   showPicture = displayPic.join("");
   photoGallery.innerHTML = showPicture;
 };
+
+// setTimeout(() => {
+//     document.getElementById("myAudio").play();
+// }, 500);
+
+
 
 
 
